8b436bb16d64a0d1188128f2691b201e1fe6029a
Noah Petherbridge <root@kirsle.net>
2015-01-05 15:26:48 -0800

    Add contributing information

--- lib/RiveScript.pm
+++ lib/RiveScript.pm
@@ -3397,7 +3397,7 @@ bot, chatbot, chatterbot, chatter bot, reply, replies, script, aiml, alpha
 
   The MIT License (MIT)
 
-  Copyright (c) 2014 Noah Petherbridge
+  Copyright (c) 2015 Noah Petherbridge
 
   Permission is hereby granted, free of charge, to any person obtaining a copy
   of this software and associated documentation files (the "Software"), to deal
